09cacb35152232d9cf2f107174d94851e206b61f
Li Wenjun <kaka@example.com>
2026-05-11 19:12:12 +0800

    remove spdlog dependency, use lightweight header-only logger
    - CMakeLists.txt: remove find_package(spdlog) and spdlog::spdlog link
    - common_utilsConfig.cmake.in: remove spdlog find_dependency
    - logger.h: replace spdlog with lightweight std::ostream-based impl
    - logger.cpp: update static member defs for header-only design

--- include/common_utils/logger.h
+++ include/common_utils/logger.h
@@ -1,7 +1,6 @@
 #pragma once
 
-#include <spdlog/spdlog.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
+#include <iostream>
 #include <memory>
 #include <string>
 
@@ -9,16 +8,47 @@ namespace common_utils {
 
 class Logger {
 public:
-    static void init(const std::string& name = "common_utils");
-    static std::shared_ptr<spdlog::logger>& get();
+    enum Level { DEBUG, INFO, WARN, ERROR };
+
+    static void init(const std::string& name = "common_utils", Level level = DEBUG);
+    static std::ostream& get(Level level = INFO);
 
 private:
-    static std::shared_ptr<spdlog::logger> s_logger;
+    static std::string s_name;
+    static Level s_level;
+    static std::string level_str(Level level);
 };
+
+inline std::string Logger::level_str(Level level) {
+    switch (level) {
+        case DEBUG: return "DEBUG";
+        case INFO:  return "INFO ";
+        case WARN:  return "WARN ";
+        case ERROR: return "ERROR";
+        default:    return "UNKWN";
+    }
+}
+
+inline void Logger::init(const std::string& name, Level level) {
+    s_name = name;
+    s_level = level;
+}
+
+inline std::ostream& Logger::get(Level level) {
+    // Always output ERROR to cerr, WARN+ to cout
+    if (level >= s_level) {
+        auto& out = (level == ERROR) ? std::cerr : std::cout;
+        out << "[" << level_str(level) << "] ";
+        return out;
+    }
+    // Return a null stream that discards everything
+    static std::ostream nullstream(nullptr);
+    return nullstream;
+}
 
 } // namespace common_utils
 
-#define CU_LOG_INFO(...)  ::common_utils::Logger::get()->info(__VA_ARGS__)
-#define CU_LOG_WARN(...)  ::common_utils::Logger::get()->warn(__VA_ARGS__)
-#define CU_LOG_ERROR(...) ::common_utils::Logger::get()->error(__VA_ARGS__)
-#define CU_LOG_DEBUG(...) ::common_utils::Logger::get()->debug(__VA_ARGS__)
+#define CU_LOG_INFO(...)  ::common_utils::Logger::get(::common_utils::Logger::INFO) << __VA_ARGS__ << std::endl
+#define CU_LOG_WARN(...)  ::common_utils::Logger::get(::common_utils::Logger::WARN) << __VA_ARGS__ << std::endl
+#define CU_LOG_ERROR(...) ::common_utils::Logger::get(::common_utils::Logger::ERROR) << __VA_ARGS__ << std::endl
+#define CU_LOG_DEBUG(...) ::common_utils::Logger::get(::common_utils::Logger::DEBUG) << __VA_ARGS__ << std::endl
--- src/logger.cpp
+++ src/logger.cpp
@@ -1,20 +1,5 @@
 #include "common_utils/logger.h"
 
-namespace common_utils {
-
-std::shared_ptr<spdlog::logger> Logger::s_logger;
-
-void Logger::init(const std::string& name) {
-    s_logger = spdlog::stdout_color_mt(name);
-    s_logger->set_level(spdlog::level::debug);
-    s_logger->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [%^%l%$] %v");
-}
-
-std::shared_ptr<spdlog::logger>& Logger::get() {
-    if (!s_logger) {
-        init();
-    }
-    return s_logger;
-}
-
-} // namespace common_utils
+// Static members are defined here (header-only inline functions need out-of-line defs)
+std::string common_utils::Logger::s_name;
+common_utils::Logger::Level common_utils::Logger::s_level;
